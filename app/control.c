84e1a43139fb25a94fff4220baeb6f861e16a97b
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2015-12-15 20:04:40 +0100

    Cleaned up control :joy:

--- app/control.c
+++ app/control.c
@@ -29,37 +29,27 @@ void button_control(int *transmitting, char *message, char *curr_char, int *msg_
             // If rightmost button (1) is pressed,
             // turn transmission on
             case 1:
-                display_string(2, "TRANSMITTING=1");
-                display_update();
                 *transmitting = 1;
                 break;
 
             case 2:
                 // If button 2 is pressed, wait for its release, then
                 // add the current character to the message buffer
-                while(get_btns() == 2){};
-                display_string(2, "MSG " + *message);
-                display_update();
+                while(get_btns() == 2);
                 add_char(message, curr_char, msg_pos, msg_max_len);
                 break;
 
             case 4:
-                while(get_btns() == 4){};
+                while(get_btns() == 4);
                 increment_char(curr_char, 1);
-                display_string(2, "INCR CHAR" + *curr_char);
-                display_update();
                 break;
 
             case 8:
-                while(get_btns() == 8){};
+                while(get_btns() == 8);
                 decrement_char(curr_char, 1);
-                display_string(2, "DECR CHAR" + *curr_char);
-                display_update();
                 break;
 
             default:
-                display_string(2, "ILLEGAL BTN CODE");
-                display_update();
                 break;
         }
     }
